db2d32547d5ac53b7aa7d1a9cf4e0463e3cfe788
JulienPetit <julienpetit1@gmail.com>
2022-09-09 16:11:38 +0200

    user app

--- Cour/index.js
+++ Cour/index.js
@@ -21,6 +21,64 @@ fetch("data.txt").then((res) => res.text());
 //.then((data) =>
 //console.log(data))
 
-fetch("data.json")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+fetch("data.json").then((res) => res.json());
+//.then((data) => console.log(data));
+
+const myHeaders = new Headers();
+
+const init = {
+  method: "GET",
+  headers: myHeaders,
+  mode: "cors",
+  cache: "default",
+};
+
+//fetch("data.json", init).then((res) => console.log(res));
+
+// CRUD
+
+const init2 = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    pseudo: "Julius",
+    message: "Yo les gens !",
+  }),
+  mode: "cors",
+  credentials: "same-origin",
+};
+
+document.querySelector("form").addEventListener("submit", () => {
+  fetch("http://localhost:3000/posts", init2).then(() =>
+    console.log("data envoyée")
+  );
+});
+//------------------------------------------------
+
+// Asynchrone
+
+setTimeout(() => {
+  //console.log("test");
+}, 2000);
+
+// promise
+
+// fetch("monlien").then((res) => res)
+
+// async/await
+
+async function fetchData() {
+  await fetch("monLien");
+  // attend que le await soit exécuté avant de faire la suite
+
+  executeFonction();
+}
+
+const fetchData2 = async () => {
+  await fetch("monLien");
+  // attend que le await soit exécuté avant de faire la suite
+
+  executeFonction();
+};
